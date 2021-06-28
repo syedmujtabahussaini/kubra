5728604833a9cc0ac55a8afd5e87add98fa878f4
Syed Mujtaba <mujtabac1@gmail.com>
2021-06-28 20:02:45 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// var num1 = +prompt("Enter any number","which table do you want")
+// console.log(typeof num1)
+
+
+
+var num1 = 10 
+var num2= 20
+
+if (num1 < 5  || num2 < 50) {
+    console.log("true")
+}
+else {
+    console.log("false")
+}
